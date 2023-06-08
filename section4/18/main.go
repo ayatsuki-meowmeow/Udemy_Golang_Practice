267177e055b47bde7e15661ac3fc8bdde6c2512d
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-06-08 22:11:13 +0900

    配列の値の宣言

--- section4/18/main.go
+++ section4/18/main.go
@@ -6,6 +6,7 @@ import "fmt"
 // 後から要素数の変更ができない → 後から要素数を変更したいときはスライスを使う
 
 func main() {
+		// 配列の宣言
 		var arr1 [3]int // 要素数が3のint型の配列、明示的な宣言で初期値なし
 		fmt.Println(arr1)
 		fmt.Printf("%T\n", arr1) // [3]int 要素数を含めて型となるから、要素数を変えられない
@@ -19,4 +20,12 @@ func main() {
 		arr4 := [...]string{"C", "D"} // 要素数を明示しない宣言
 		fmt.Println(arr4)
 		fmt.Printf("%T\n", arr4) // [2]string
+
+		// 配列の値の取得
+		fmt.Println(arr2[0]) // arr2の最初の値を取れる、[0]はインデックス番号
+		fmt.Println(arr2[1])
+		fmt.Println(arr2[2])
+		fmt.Println(arr2[2-1]) // arr2[1]と同じ
+		fmt.Println(arr2[2-2])
+		fmt.Println(arr3[3-1]) // [要素数-1]にすると、配列の最後の要素を取得できる
 }
