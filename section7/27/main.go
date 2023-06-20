b0fd9287c7031e99f9855601a3104674e9174f24
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-06-20 09:18:24 +0900

    関数

--- section7/27/main.go
+++ section7/27/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 関数
 
-// 引数の型を指定して、返り値の型をしてする
+// 引数の型を指定して、返り値の型を指定する
 func Plus(x int, y int) int {
 		return x + y
 }
@@ -21,6 +21,18 @@ func Div(x, y int) (int, int) {
 		return q, r
 }
 
+// 返り値の変数名を宣言する(ここだとresult)ことで、returnの中身を省略できる
+func Double(price int) (result int) {
+		result = price * 2
+		return
+}
+
+// 返り値のない関数
+func Noreturn() {
+		fmt.Println("No Return")
+		return
+}
+
 func main() {
 		i := Plus(1, 2)
 		fmt.Println(i)
@@ -29,5 +41,17 @@ func main() {
 		fmt.Println(i2)
 
 		var i3, i4 int = Div(4, 3)
+		// fmt.Println(i3)だとエラーになる
+		// 変数宣言した変数は必ず使わないといけない
 		fmt.Println(i3, "余り", i4)
+
+		// 返り値の破棄
+		// 使いたくない変数は_にする事で破棄できる
+		var i5, _ int = Div(4, 3)
+		fmt.Println(i5) // エラーにならない
+
+		var i6 int = Double(1000)
+		fmt.Println(i6)
+
+		Noreturn() // No Return
 }
