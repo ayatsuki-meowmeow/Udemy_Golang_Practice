99a2d40b27248d6edd6132dd2c407d896cc5999c
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-07-10 21:34:53 +0900

    make len cap

--- section9/46/main.go
+++ section9/46/main.go
@@ -20,4 +20,30 @@ func main() {
 		// append(スライス, 追加する要素1, 追加する要素2, ...)
 		sl = append(sl, 400, 500, 600)
 		fmt.Println(sl) // [100 200 300 400 500 600]
+
+		// make関数を使ってスライスを作成する
+		// make(スライスの型, スライスの長さ(要素数), スライスの容量)
+		sl2 := make([]int, 5)
+		fmt.Println(sl2) // [0 0 0 0 0]
+
+		// len関数でスライスの要素数を取得する
+		fmt.Println(len(sl2)) // 5
+
+		// cap関数でスライスの容量を取得する
+		fmt.Println(cap(sl2)) // 5
+
+		// 容量10のスライスを作成する
+		// プログラムのメモリーを気にする開発を行う場合は、容量を指定してスライスを作成する
+		sl3 := make([]int, 5, 10)
+		fmt.Println(sl3) // [0 0 0 0 0]
+		fmt.Println(len(sl3)) // 5
+		fmt.Println(cap(sl3)) // 10
+
+		// 容量を超えた要素を追加する
+		// 容量を超えた要素を追加すると、容量が自動的に倍増される
+		// 倍増するからパフォーマンスへの影響がある
+		sl3 = append(sl3, 1, 2, 3, 4, 5, 6, 7)
+		fmt.Println(sl3) // [0 0 0 0 0 1 2 3 4 5 6 7]
+		fmt.Println(len(sl3)) // 12
+		fmt.Println(cap(sl3)) // 20
 }
